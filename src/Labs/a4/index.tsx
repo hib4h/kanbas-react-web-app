fcc55e2159acc0b62bcc705ff9a44514f4de2324
hib4h <134744232+hib4h@users.noreply.github.com>
2024-03-20 23:12:06 -0400

    implemented lab 4

---
+++ src/Labs/a4/index.tsx
@@ -0,0 +1,33 @@
+import ReduxExamples from "./ReduxExamples";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+
+function Assignment4() {
+  function sayHello() {
+    alert("Hello");
+  }
+  return (
+    <div>
+      <h1>Assignment 4</h1>
+      <ReduxExamples/>
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject/>
+      <Counter/>
+      <BooleanStateVariables/>
+      <StringStateVariables/>
+      <DateStateVariable/>
+      <ObjectStateVariable/>
+      <ArrayStateVariable/>
+      <ParentStateComponent/>
+    </div>
+  );
+}
+export default Assignment4;
+
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,19 +1,23 @@
 import { Route, Routes } from "react-router";
 import { Link } from "react-router-dom";
 import Assignment3 from "./a3";
-// import Assignment4 from "./Assignment4";
+import Assignment4 from "./a4";
 import Nav from "../Nav";
+import store from "./store";
+import { Provider } from "react-redux";
 
 function Labs() {
   return (
     <>
       <Nav />
+      <Provider store={store}>
       <h1>Labs</h1>
       <Link to="a3">Assignment 3</Link> | <Link to="a4">Assignment 4</Link>
       <Routes>
         <Route path="a3/*" element={<Assignment3 />} />
-        {/* <Route path="a4" element={<Assignment4 />} /> */}
+        <Route path="a4" element={<Assignment4 />} />
       </Routes>
+      </Provider>
     </>
   );
 }
